96fe41ef5ebbf7d6aa2548e5569537b16a57f893
antondubek <makepeaceanthony@gmail.com>
2017-07-29 17:09:08 +0000

    Added Library File

---
+++ Main.py
@@ -0,0 +1,15 @@
+
+####
+## Import libraries
+####
+import RPi.GPIO as GPIO
+import time
+
+
+
+####
+## Initialisation of Pins
+####
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
