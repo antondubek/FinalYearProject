90be0c330c1a1cd07b7efd1224bf7736e3bc38ed
Anthony Makepeace <makepeaceanthony@gmail.com>
2017-07-31 14:36:40 +0100

    Added simple take picture function

--- Images.py
+++ Images.py
@@ -1,4 +1,27 @@
 """
 Author: Anthony Makepeace
 File: Images
+About: File to include functions relating to taking and
+uploading images to a location where a web application can find them.
 """
+
+####
+## Import libraries
+####
+import time
+import os
+
+####
+## Initialisation of Pins
+####
+
+####
+## Functions
+####
+def takePicture():
+    print ("Taking Picture")
+    os.system("cd /home/pi/Documents/TestProjects/WebcamPics | fswebcam image.jpg")
+
+####
+## Main
+####
